3582c107d6aa6da1b861f75b4e7c3c896786b5a1
yfsoftcom <yfsoftcom@126.com>
2020-04-16 12:30:29 +0800

    the inherit struct

---
+++ oop/main.go
@@ -0,0 +1,29 @@
+/**
+
+
+**/
+package main
+
+import "fmt"
+
+type BaseType string
+
+func (b BaseType) sayHi() {
+	fmt.Println("greet from ", b)
+}
+
+type InheritTypeA BaseType
+
+type InheritTypeB struct {
+	BaseType
+}
+
+func main() {
+	itA := InheritTypeA("it A")
+	itA.sayHi() // compile error: the inheritType dont inherit the sayhi method.
+
+	itB := InheritTypeB("it B")
+	itB.sayHi() // here is ok
+
+	fmt.Println("hello golang!")
+}
